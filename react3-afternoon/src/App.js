ec55cfc67355456e3f175afde50f78d955d09209
PhillipRose <philliprosejob@gmail.com>
2020-10-15 15:56:48 -0600

    had to remake this due to a git error, just copying over

---
+++ react3-afternoon/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import data from './data'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <data/>
+    </div>
+  );
+}
+
+export default App;
